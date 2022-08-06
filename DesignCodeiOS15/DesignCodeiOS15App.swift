cf6d7d99527eecfc803f724316c5a4d3b41e02d2
Todd James <todd@itodd.co>
2022-08-06 08:30:48 -0400

    Initial Commit

---
+++ DesignCodeiOS15/DesignCodeiOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS15App.swift
+//  DesignCodeiOS15
+//
+//  Created by Todd James on 8/6/22.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
