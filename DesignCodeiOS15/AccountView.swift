88c20b5695db11d3dff2d944e0045fe1182815b7
Todd James <todd@itodd.co>
2022-08-07 11:30:51 -0400

    Lesson 9

--- DesignCodeiOS15/AccountView.swift
+++ DesignCodeiOS15/AccountView.swift
@@ -38,11 +38,46 @@ struct AccountView: View {
           .padding()
           
           Section {
-            Label("Settings", systemImage: "gear")
-            Label("Billing", systemImage: "creditcard")
-            Label("Help", systemImage: "questionmark")
+            NavigationLink(destination: ContentView()) {
+              Label("Settings", systemImage: "gear")
+            }
+            
+            NavigationLink {
+              Text("Billing")
+            } label: {
+              Label("Billing", systemImage: "creditcard")
+            }
+            
+            NavigationLink { ContentView() } label: {
+              Label("Help", systemImage: "questionmark")
+            }
+            
           }
+          .accentColor(.primary)
           .listRowSeparatorTint(.blue)
+          .listRowSeparator(.hidden)
+          
+          Section {
+            Link(destination: URL(string: "https://apple.com")!) {
+              HStack {
+                Label("Apple", systemImage: "house")
+                Spacer()
+                Image(systemName: "link")
+                  .foregroundColor(.secondary)
+              }
+            }
+            
+            Link(destination: URL(string: "https://designcode.io")!) {
+              HStack {
+                Label("DesignCode.io", systemImage: "tv")
+                Spacer()
+                Image(systemName: "link")
+                  .foregroundColor(.secondary)
+              }
+            }
+            
+          }
+          .accentColor(.primary)
           .listRowSeparator(.hidden)
           
         }
