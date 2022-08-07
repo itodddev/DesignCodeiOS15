451345f3e531bc9022dd7ab27a6b6039afb3369a
Todd James <todd@itodd.co>
2022-08-07 10:54:47 -0400

    Lesson 8

--- DesignCodeiOS15/AccountView.swift
+++ DesignCodeiOS15/AccountView.swift
@@ -11,10 +11,36 @@ struct AccountView: View {
     var body: some View {
       NavigationView {
         List {
+          VStack(spacing: 8) {
+            Image(systemName: "person.crop.circle.fill.badge.checkmark")
+              .font(.system(size: 32))
+              .symbolRenderingMode(.palette)
+              .foregroundStyle(.blue, .blue.opacity(0.3))
+              .padding()
+              .background(Circle().fill(.ultraThinMaterial))
+              .background(
+                Image(systemName: "hexagon")
+                  .symbolVariant(.fill)
+                  .foregroundColor(.blue)
+                  .font(.system(size: 200))
+                  .offset(x: -50, y: -100)
+            )
+            Text("Todd James")
+              .font(.title.weight(.semibold))
+            HStack {
+              Image(systemName: "location")
+                .imageScale(.small)
+              Text("United States")
+                .foregroundColor(.secondary)
+            }
+          }
+          .frame(maxWidth: .infinity)
+          .padding()
+          
           Section {
-            Text("Settings")
-            Text("Billing")
-            Text("Help")
+            Label("Settings", systemImage: "gear")
+            Label("Billing", systemImage: "creditcard")
+            Label("Help", systemImage: "questionmark")
           }
           .listRowSeparatorTint(.blue)
           .listRowSeparator(.hidden)
@@ -27,6 +53,10 @@ struct AccountView: View {
 
 struct AccountView_Previews: PreviewProvider {
     static var previews: some View {
+      Group {
         AccountView()
+        AccountView()
+          .preferredColorScheme(.dark)
+      }
     }
 }
