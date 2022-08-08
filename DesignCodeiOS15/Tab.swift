73e8d85181e544d451d261c73a1d16c7d1a8b353
Todd James <todd@itodd.co>
2022-08-08 18:29:34 -0400

    Lesson 18

--- DesignCodeiOS15/Tab.swift
+++ DesignCodeiOS15/Tab.swift
@@ -12,12 +12,20 @@ struct TabItem: Identifiable { // for looping
   var id = UUID() // default value, dont have to add as param, auto-generates unique id
   var text: String
   var icon: String
+  var tab: Tab
   
 }
 
 var tabItems = [
-  TabItem(text: "Learn Now", icon: "house"),
-  TabItem(text: "Explore", icon: "magnifyingglass"),
-  TabItem(text: "Notifications", icon: "bell"),
-  TabItem(text: "Library", icon: "rectangle.stack")
+  TabItem(text: "Learn Now", icon: "house", tab: .home),
+  TabItem(text: "Explore", icon: "magnifyingglass", tab: .explore),
+  TabItem(text: "Notifications", icon: "bell", tab: .notifications),
+  TabItem(text: "Library", icon: "rectangle.stack", tab: .library)
 ]
+
+enum Tab: String {
+  case home
+  case explore
+  case notifications
+  case library
+}
--- DesignCodeiOS15/TabBar.swift
+++ DesignCodeiOS15/TabBar.swift
@@ -8,17 +8,35 @@
 import SwiftUI
 
 struct TabBar: View {
-    var body: some View {
-      ZStack(alignment: .bottom) {
-        // Contentview takes whole screen
-        ContentView()
-          .frame(maxWidth: .infinity, maxHeight: .infinity)
+  
+  @State var selectedTab: Tab = .home
+  
+  var body: some View {
+    ZStack(alignment: .bottom) {
+      
+      Group {
+        switch(selectedTab) {
+          case .home:
+            // Contentview takes whole screen
+            ContentView()
+          case .explore:
+            AccountView()
+          case .notifications:
+            EmptyView()
+          case .library:
+            EmptyView()
+        }
+      }
+      .frame(maxWidth: .infinity, maxHeight: .infinity) // add frame to Group
+      
+      //HStack ontop of ContentView, but aligned to ZStack .bottom
+      HStack {
+        Spacer()
         
-        //HStack ontop of ContentView, but aligned to ZStack .bottom
-        HStack {
-          Spacer()
-          
-          ForEach(tabItems) { item in
+        ForEach(tabItems) { item in
+          Button {
+            selectedTab = item.tab // on button click set selectedTab to current tab
+          } label: {
             VStack(spacing: 0) {
               Image(systemName: item.icon)
                 .symbolVariant(.fill)
@@ -29,23 +47,29 @@ struct TabBar: View {
                 .lineLimit(1)
             } //: VSTACK
             .frame(maxWidth: .infinity)
-            
-          } //: FOREACH
-        } //: HSTACK
-        .padding(.horizontal, 8)        .padding(.top, 14)
-        .frame(height: 88, alignment: .top)
-        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 34, style: .continuous))
-        .strokeStyle(cornerRadius: 34)
-        .frame(maxHeight: .infinity, alignment: .bottom)
-        .ignoresSafeArea()
-        
-      }
+          } //: BUTTON
+          .foregroundStyle(selectedTab == item.tab ? .primary : .secondary) // if selected tab = item.tab change color
+        } //: FOREACH
+      } //: HSTACK
+      .padding(.horizontal, 8)        .padding(.top, 14)
+      .frame(height: 88, alignment: .top)
+      .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 34, style: .continuous))
+      .strokeStyle(cornerRadius: 34)
+      .frame(maxHeight: .infinity, alignment: .bottom)
+      .ignoresSafeArea()
+      
     }
+  }
 }
 
 struct TabBar_Previews: PreviewProvider {
-    static var previews: some View {
+  static var previews: some View {
+    Group {
       TabBar()
         .previewDevice("iPhone 12 Pro Max")
+      TabBar()
+        .preferredColorScheme(.dark)
+        .previewDevice("iPhone 12 Pro Max")
     }
+  }
 }
