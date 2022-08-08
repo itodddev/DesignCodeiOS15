433a5384e4bec48dca7e75eaf33d0911a1bebc76
Todd James <todd@itodd.co>
2022-08-08 19:13:53 -0400

    Lesson 19

--- DesignCodeiOS15/TabBar.swift
+++ DesignCodeiOS15/TabBar.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct TabBar: View {
   
   @State var selectedTab: Tab = .home
-  
+  @State var color: Color = .teal
+
   var body: some View {
     ZStack(alignment: .bottom) {
       
@@ -35,7 +36,10 @@ struct TabBar: View {
         
         ForEach(tabItems) { item in
           Button {
-            selectedTab = item.tab // on button click set selectedTab to current tab
+            withAnimation(.spring(response: 0.3, dampingFraction: 0.7)) {
+              selectedTab = item.tab // on button click set selectedTab to current tab
+              color = item.color
+            }
           } label: {
             VStack(spacing: 0) {
               Image(systemName: item.icon)
@@ -49,11 +53,58 @@ struct TabBar: View {
             .frame(maxWidth: .infinity)
           } //: BUTTON
           .foregroundStyle(selectedTab == item.tab ? .primary : .secondary) // if selected tab = item.tab change color
+          .blendMode(selectedTab == item.tab ? .overlay : .normal)
         } //: FOREACH
       } //: HSTACK
-      .padding(.horizontal, 8)        .padding(.top, 14)
+      .padding(.horizontal, 8)
+      .padding(.top, 14)
       .frame(height: 88, alignment: .top)
       .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 34, style: .continuous))
+      .background( // circle behind the material sheet
+        HStack {
+          if selectedTab == .library { Spacer() }
+          if selectedTab == .explore { Spacer() }
+          if selectedTab == .notifications {
+            Spacer()
+            Spacer()
+          }
+          
+          Circle().fill(color).frame(width: 88)
+          
+          if selectedTab == .home { Spacer() }
+          if selectedTab == .explore {
+            Spacer()
+            Spacer()
+          }
+          if selectedTab == .notifications { Spacer() }
+        }
+        .padding(.horizontal, 8) // mimick horizontal padding on above HStack to position cicle behind tab
+      )
+      .overlay( // line at top
+        HStack {
+          if selectedTab == .library { Spacer() }
+          if selectedTab == .explore { Spacer() }
+          if selectedTab == .notifications {
+            Spacer()
+            Spacer()
+          }
+          
+          Rectangle()
+            .fill(color)
+            .frame(width: 28, height: 5)
+            .cornerRadius(3)
+            .frame(width: 88) // same frame size to align with circle
+            .frame(maxHeight: .infinity, alignment: .top)
+          
+          if selectedTab == .home { Spacer() }
+          if selectedTab == .explore {
+            Spacer()
+            Spacer()
+          }
+          if selectedTab == .notifications { Spacer() }
+        }
+        .padding(.horizontal, 18) // mimick horizontal padding on above HStack to position cicle behind tab
+      )
       .strokeStyle(cornerRadius: 34)
       .frame(maxHeight: .infinity, alignment: .bottom)
       .ignoresSafeArea()
