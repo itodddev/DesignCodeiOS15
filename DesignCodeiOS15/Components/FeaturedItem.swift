549fb27c2aa59a4b2df88622bb0d7a73d25f911b
Todd James <todd@itodd.co>
2022-08-11 10:39:38 -0400

    Lesson 27

--- DesignCodeiOS15/Components/FeaturedItem.swift
+++ DesignCodeiOS15/Components/FeaturedItem.swift
@@ -8,10 +8,13 @@
 import SwiftUI
 
 struct FeaturedItem: View {
+  
+  var course: Course = courses[0]
+  
     var body: some View {
         VStack(alignment: .leading, spacing: 8.0) {
           Spacer()
-          Image("Logo 2")
+          Image(course.logo)
             .resizable(resizingMode: .stretch)
             .aspectRatio(contentMode: .fit)
             .frame(width: 26.0, height: 26.0)
@@ -19,15 +22,16 @@ struct FeaturedItem: View {
             .padding(9)
             .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 16, style: .continuous))
             .strokeStyle(cornerRadius: 16.0)
-          Text("SwiftUI for iOS 15")
+          Text(course.title)
             .font(.largeTitle)
             .fontWeight(.bold)
+            .lineLimit(1)
             .foregroundStyle(.linearGradient(colors: [.primary, .primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
-          Text("20 sections - 3 hours".uppercased())
+          Text(course.subtitle.uppercased())
             .font(.footnote)
             .fontWeight(.semibold)
             .foregroundStyle(.secondary)
-          Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
+          Text(course.text)
             .font(.footnote)
             .multilineTextAlignment(.leading)
             .lineLimit(2)
@@ -41,12 +45,13 @@ struct FeaturedItem: View {
         .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
         .strokeStyle()
         .padding(.horizontal, 20)
-        .background(
-          Image("Blob 1")
-            .offset(x: 250, y: -100)
-        )
+// Moved to HomeView - background removed from card and is now on frame of FeaturedItem
+//        .background(
+//          Image("Blob 1")
+//            .offset(x: 250, y: -100)
+//        )
         .overlay(
-          Image("Illustration 5")
+          Image(course.image)
             .resizable()
             .aspectRatio(contentMode: .fit)
             .frame(height: 230)
@@ -57,6 +62,6 @@ struct FeaturedItem: View {
 
 struct FeaturedItem_Previews: PreviewProvider {
     static var previews: some View {
-        FeaturedItem()
+        FeaturedItem(course: courses[0])
     }
 }
--- DesignCodeiOS15/Views/HomeView.swift
+++ DesignCodeiOS15/Views/HomeView.swift
@@ -12,18 +12,19 @@ struct HomeView: View {
   @State var hasScrolled = false
   
   var body: some View {
+    ZStack {
+      
+      Color("Background")
+        .ignoresSafeArea()
+      
       ScrollView {
         
-        GeometryReader { proxy in
-          //Text("\(proxy.frame(in: .named("scroll")).minY)")
-          Color.clear.preference(key: ScrollPreferenceKey.self, value: proxy.frame(in: .named("scroll")).minY)
-        }
-        .frame(height: 0)
+        scrollDetection
         
-        FeaturedItem()
+        featured
         
         Color.clear.frame(height: 1000) // to get scrollbars
-  
+        
       }
       .coordinateSpace(name: "scroll")
       .onPreferenceChange(ScrollPreferenceKey.self, perform: { value in
@@ -40,14 +41,37 @@ struct HomeView: View {
       })
       .overlay(
         NavigationBar(title: "Featured", hasScrolled: $hasScrolled)
-          
+        
       )
     }
+  }
+  
+  var scrollDetection: some View {
+    GeometryReader { proxy in
+      //Text("\(proxy.frame(in: .named("scroll")).minY)")
+      Color.clear.preference(key: ScrollPreferenceKey.self, value: proxy.frame(in: .named("scroll")).minY)
+    }
+    .frame(height: 0)
+  }
+  
+  var featured: some View {
+    TabView {
+      ForEach(courses) { item in
+        FeaturedItem(course: item)
+      }
+    }
+    .tabViewStyle(.page(indexDisplayMode: .never))
+    .frame(height: 430)
+    .background(
+      Image("Blob 1")
+        .offset(x: 250, y: -100)
+    )
+  }
 }
 
 struct HomeView_Previews: PreviewProvider {
-    static var previews: some View {
-      HomeView()
-        .previewDevice("iPhone 12 Pro Max")
-    }
+  static var previews: some View {
+    HomeView()
+      .previewDevice("iPhone 12 Pro Max")
+  }
 }
